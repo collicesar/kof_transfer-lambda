3d43bfdf9dcee93447af767392f107f6f16d77bc
Diana Milena Herrera <diana.herrera@externos.digitalfemsa.com>
2023-09-19 11:26:17 -0500

    Segunda lambda que se logea y recibe la URL firmada para enviar a un archivo al S3 de Gravty

---
+++ FemsaKofSendBatchFile/Models/ResponseLogin.cs
@@ -0,0 +1,14 @@
+﻿using System.Data;
+using Newtonsoft.Json;
+
+namespace FemsaKofSendBatchFile.Models
+{
+    public class ResponseLogin
+    {
+        [JsonProperty("token")]
+        public string? Token { get; set; }
+
+        [JsonProperty("success")]
+        public bool Success { get; set; }
+    }
+}
---
+++ FemsaKofSendBatchFile/Validators/ValidEnvironmentVariables.cs
@@ -0,0 +1,18 @@
+﻿using FemsaKofSendBatchFile.Validators.Abstractions;
+
+namespace FemsaKofSendBatchFile.Validators
+{
+    public class ValidEnvironmentVariables : IValidEnvironmentVariables
+    {
+        bool IValidEnvironmentVariables.IsValidEnvironmentVariables(string gravtyLoginUrl, string gravtyGetSignedUrl, string apiKey, string userName, string password)
+        {
+            if (string.IsNullOrEmpty(gravtyLoginUrl) || !Uri.IsWellFormedUriString(gravtyLoginUrl, UriKind.Absolute))
+                return false;
+            if (string.IsNullOrEmpty(gravtyGetSignedUrl) || !Uri.IsWellFormedUriString(gravtyGetSignedUrl, UriKind.Absolute))
+                return false;
+            if(string.IsNullOrEmpty(apiKey) || string.IsNullOrEmpty(userName) || string.IsNullOrEmpty(password))
+                return false;
+            return true;
+        }
+    }
+}
