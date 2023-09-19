37eeb93b009977ecae1435be946c1658c5a26844
Diana Milena Herrera <diana.herrera@externos.digitalfemsa.com>
2023-09-19 10:33:05 -0500

    Revert "Mover arcivos para que queden en la raiz del repositorio."
    This reverts commit 2dbe76f23668fb6e266bee6ba298c0e89ffe5967.

--- Kof_Transfer-Lambda/FemsaKofSendBatchFile/Validators/Abstractions/IValidEnvironmentVariables.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace FemsaKofSendBatchFile.Validators.Abstractions
-{
-    public interface IValidEnvironmentVariables
-    {
-        bool IsValidEnvironmentVariables(string gravtyLoginUrl, string gravtyGetSignedUrl, string apiKey, string userName, string password);        
-    }
-}
